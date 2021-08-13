0d3333baf4c74587abccc0665b6c8459b819094c
정희석(JEONG HEE SEOK)/AI플랫폼그룹/SK <heeseok.jeong@skcc.com>
2021-08-14 00:11:48 +0900

    first commit

---
+++ explain.sh
@@ -0,0 +1,1 @@
+curl -H "Host: runtime-929-mdl-c5de38-for-explain.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v1/models/runtime-929-mdl-c5de38-for-explain:explain -d @./input.json
