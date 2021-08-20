70eaa7477a4b342ae169be42a152d1501c67ffa4
정희석(JEONG HEE SEOK)/AI플랫폼그룹/SK <heeseok.jeong@skcc.com>
2021-08-20 17:08:44 +0900

    update

--- explain.sh
+++ explain.sh
@@ -1,1 +1,4 @@
-curl -H "Host: runtime-929-mdl-c5de38-for-explain.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v1/models/runtime-929-mdl-c5de38-for-explain:explain -d @./input.json
+INPUT=@./input_broccoli.json
+#INPUT=@./input.json
+
+curl -H "Host: runtime-929-mdl-c5de38-for-explain.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v1/models/runtime-929-mdl-c5de38-for-explain:explain -d ${INPUT}
--- predict.sh
+++ predict.sh
@@ -1,1 +1,3 @@
-curl -H "Host: runtime-929-mdl-c5de38-for-explain.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v1/models/runtime-929-mdl-c5de38-for-explain:predict -d @./input.json
+INPUT=@./input_broccoli.json
+
+curl -H "Host: runtime-929-mdl-c5de38-for-explain.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v1/models/runtime-929-mdl-c5de38-for-explain:predict -d ${INPUT}
