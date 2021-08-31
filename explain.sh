bb0cb55d9acdb11385b50d3949bd339507d94fcf
jhs0318 <jhs0318@gmail.com>
2021-08-31 15:42:33 +0900

    update

--- explain.sh
+++ explain.sh
@@ -2,5 +2,6 @@
 # export MODEL=runtime-929-mdl-c5de38-for-explain
 export INPUT=@./input_movie.json
 export MODEL=runtime-929-mdl-b17746-for-explain
+export HOST="Host: ${MODEL}.test-jhs-r.example.com"
 
-curl -H "Host: ${MODEL}.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v2/models/${MODEL}/explain -d ${INPUT}
+curl -H ${HOST} http://istio-ingressgateway.istio-system.svc.cluster.local/v2/models/${MODEL}/explain -d ${INPUT}
