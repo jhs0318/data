729e6acba3807b888b8525471bfd94ebd61016dd
jhs0318 <jhs0318@gmail.com>
2021-08-31 15:45:45 +0900

    update

--- predict.sh
+++ predict.sh
@@ -1,6 +1,6 @@
 # export INPUT=@./input_broccoli.json
 # export MODEL=runtime-929-mdl-c5de38-for-explain
-export INPUT=@./input_movie.json
+export INPUT=@./input_movie_for_predict.json
 export MODEL=runtime-929-mdl-b17746-for-explain
 
 curl -H "Host: ${MODEL}.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v2/models/${MODEL}/infer -d ${INPUT}
