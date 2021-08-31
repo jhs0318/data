c236a4ff995e13cb679ea66382e5a6458fe382d5
jhs0318 <jhs0318@gmail.com>
2021-08-31 15:38:44 +0900

    update

--- explain.sh
+++ explain.sh
@@ -1,4 +1,6 @@
-INPUT=@./input_broccoli.json
-#INPUT=@./input.json
+# export INPUT=@./input_broccoli.json
+# export MODEL=runtime-929-mdl-c5de38-for-explain
+export INPUT=@./input_movie.json
+export MODEL=runtime-929-mdl-b17746-for-explain
 
-curl -H "Host: runtime-929-mdl-c5de38-for-explain.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v1/models/runtime-929-mdl-c5de38-for-explain:explain -d ${INPUT}
+curl -H "Host: ${MODEL}.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v2/models/${MODEL}/explain -d ${INPUT}
--- predict.sh
+++ predict.sh
@@ -1,3 +1,6 @@
-INPUT=@./input_broccoli.json
+# export INPUT=@./input_broccoli.json
+# export MODEL=runtime-929-mdl-c5de38-for-explain
+export INPUT=@./input_movie.json
+export MODEL=runtime-929-mdl-b17746-for-explain
 
-curl -H "Host: runtime-929-mdl-c5de38-for-explain.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v1/models/runtime-929-mdl-c5de38-for-explain:predict -d ${INPUT}
+curl -H "Host: ${MODEL}.test-jhs-r.example.com" http://istio-ingressgateway.istio-system.svc.cluster.local/v2/models/${MODEL}/infer -d ${INPUT}
